9e0afedd46bfdb9627673324406dcc2e614f0cf4
Subhendu De <subhendude@gmail.com>
2021-06-29 15:51:05 +0530

    add few commands

--- command.bat
+++ command.bat
@@ -8,3 +8,8 @@ REM adds both the projects to the solution
 dotnet sln add .\eKart.Api\eKart.Api.csproj
 dotnet restore .\eKart.Api\eKart.Api.csproj
 dotnet build .\eKart.Api\eKart.Api.csproj
+
+dotnet add package Swashbuckle.AspNetCore
+
+dotnet build --project .\eKart.Api\eKart.Api.csproj
+dotnet run --project .\eKart.Api\eKart.Api.csproj
